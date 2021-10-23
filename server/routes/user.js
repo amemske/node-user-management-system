fd03f6ba9a9012f5b8c320e6b63f16657216ff82
Antony Memia <antony@olywave.com>
2021-10-24 01:22:57 +0300

    adding new records

--- server/routes/user.js
+++ server/routes/user.js
@@ -5,6 +5,8 @@ const userController = require("../controllers/userController");
 //get the route, add contoller and get it's methods e.g. view
 router.get("/", userController.view);
 router.post("/", userController.find);
+router.get("/adduser", userController.form);
+router.post("/adduser", userController.create);
 
 //export
 module.exports = router;
